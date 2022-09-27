2fec540f605b5ed2bb4d669473054a583ef33feb
{SunCreation} <{cake0103@nate.com}>
2022-09-27 18:38:31 +0900

    feat: gender bar

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from PIL import Image
-st.title('hi!!')
+from PIL import Image, ImageSequence
+
+
+st.title('Sidabari Project ')
 
 st.write('# Gender')
 
-man_woman = st.slider('gender', 0, 30, 15)
+man_woman = st.slider('gender', 0, 80, 15)
 # filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 def open_img(sexibility):
-    img_path = f'img/img{"%0.2d" %sexibility}.png'
+    img_path = f'gender/frame{"%0.2d" %sexibility}.png'
     img = Image.open(img_path)
     return img
 
@@ -31,6 +33,8 @@ def open_img(sexibility):
     return img
 
 st.image(open_img(man_woman))
+# st.image(frames[man_woman])
+
 
 st.subheader(f'Gender of the one: {"%0.3f" %(man_woman/30 * 100)} % woman')
 
@@ -48,5 +52,4 @@ def open_img(sexibility):
 st.image(open_img(man_woman))
 
 st.subheader(f'Gender of the one: {"%0.3f" %(man_woman/30 * 100)} % woman')
-
 
